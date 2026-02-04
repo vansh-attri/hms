4f5c8d92632da590e2bb768bb94c9cdac6fc210d
Vansh Attri <vanshattri123@gmail.com>
2026-02-04 22:49:10 +0530

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PFT7XM5B');`}
         </Script>
+        
+        {/* Google tag (gtag.js) - Google Analytics */}
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-541QDNKMQZ" 
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-541QDNKMQZ');`}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
